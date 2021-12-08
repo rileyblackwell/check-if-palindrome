3d8d8a243c1990d9b774d8f5cf909e4bb47f1b21
Riley Blackwell <rileyblackwell5@gmail.com>
2021-12-08 15:54:06 -0500

    initial commit

---
+++ check_palindrome.py
@@ -0,0 +1,25 @@
+def is_pal(word):
+    """ -assumes that word is a string  
+    
+    """
+    if len(word) == 0 or len(word) == 1:
+        return True
+    elif word[0] != word[-1]:
+        return False
+    else:
+        return is_pal(word[1:-1])
+
+# test case: palindrome
+word = 'anabana'
+
+print(is_pal(word))
+
+# test case:  not palindrome
+word = 'banana'
+
+print(is_pal(word))
+
+# expected output: 
+# True
+# False
+
