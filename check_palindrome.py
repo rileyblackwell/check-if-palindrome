35ae8ce43128943d488485b392659076c26f3b96
Riley Blackwell <rileyblackwell5@gmail.com>
2021-12-26 09:05:55 -0500

    improved documentation

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,6 +1,8 @@
 def is_pal(word):
-    """ -assumes that word is a string  
-    
+    """ -assumes 
+            word is a string  
+            word maintains case sensitivity throughout (all upper or all lower)
+            word contains no spaces 
     """
     if len(word) == 0 or len(word) == 1:
         return True
@@ -9,17 +11,21 @@ def is_pal(word):
     else:
         return is_pal(word[1:-1])
 
-# test case: palindrome
+
+# 2 required test cases palindrome True and palindrome False
+# test case: palindrome True
 word = 'anabana'
 
 print(is_pal(word))
 
-# test case:  not palindrome
+# test case: palindrome False
 word = 'banana'
 
 print(is_pal(word))
 
-# expected output: 
+# expected output from test cases: 
 # True
 # False
+
+
 
